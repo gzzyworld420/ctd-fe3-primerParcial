6ce964cd246615dbf75cfa00f647ee8a55ca390b
Alejandra Fuentealba <alejandrafuentealba@MacBook-Air-de-Alejandra.local>
2023-11-01 20:40:57 -0300

    subiendo todos los cambios del primer parcial

---
+++ primer-parcial-fe3/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { Cards } from './Components/Cards'
+import { Form } from './Components/Form'
+
+let autoid = 0
+
+export function App() {
+  const [students, setStudents] = useState([])
+
+  const addStudent = info => {
+    setStudents([...students, { info, id: autoid++ }])
+  }
+
+  const deleteStudent = id => {
+    setStudents(students.filter(student => student.id !== id))
+  }
+
+  return (
+    <>
+      <h1 className='mb-4'>Carga de estudiantes</h1>
+      <Form addStudent={addStudent} />
+      <Cards students={students} deleteStudent={deleteStudent} />
+    </>
+  )
+}
